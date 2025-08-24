9797210e0a4599c4038c11b4891037f5f3e0bb72
BoomBidiBuyBuy <galinskyifmo@gmail.com>
2025-08-24 12:55:29 +0000

    add the /message endpoint

--- src/server.py
+++ src/server.py
@@ -1,77 +1,64 @@
-import asyncio
+import json
 import logging
+from typing import Annotated
 
 from fastmcp import FastMCP
 from langchain_core.messages import HumanMessage, SystemMessage
+from starlette.responses import JSONResponse
 
 import agent
-import envs
+from envs import MCP_HOST, MCP_PORT
 
-mcp = FastMCP(name="mcp-agent-worker")
+mcp_server = FastMCP(name="mcp-agent-worker")
 
 
+logger = logging.getLogger(__name__)
 # Configure logging
-logger = logging.getLogger("mcp_agent_worker")
-if not logging.getLogger().handlers:
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s: %(message)s"
-    )
 logger.info("MCP Agent Worker initialized")
 
 
-@mcp.tool
-def execute_plan(str_json_plan: str) -> str:
-    """
-    Execute a plan using the agent help.
+@mcp_server.tool
+async def execute_plan(
+    str_json_plan: Annotated[str, "A JSON string representing the plan to execute."],
+) -> Annotated[str, "The result of the plan execution."]:
+    """Execute a plan using the agent help."""
+    logger.info(f"Executing plan ={str_json_plan}")
 
-    Args:
-        str_json_plan: A JSON string representing the plan to execute.
-        Plan contains list / sequence actions.
-        Action is an MCP tool call that contains endpoint of MCP service to call, tool name to call,
-        arguments, and condition when the call to execute.
+    # retrieve MCP service addresses from the plan
+    # with goal to connect agent to them to get proper tools
+    parsed_json = json.loads(str_json_plan)
+    thread_id = parsed_json.get("thread_id")
+    # ext_mcp_servers = {record["mcp-service-endpoint"] for record in parsed_json.values()}
+    #
+    # dict_ext_mcp_servers = dict()
+    # for inx, value in enumerate(ext_mcp_servers):
+    #    dict_ext_mcp_servers[f"name{inx}"] = {"transport": "streamable_http", "url": value}
 
-        The JSON has the following structure:
-        {
-            "unique_action_id_1": {
-                "mcp-service-name": "name-of-the-mcp-service-to-call",
-                "mcp-tool-name": "name-of-the-mcp-tool-to-call",
-                "mcp-tool-arguments": {
-                    "arg-name": "arg-value"
-                },
-                "condition": "executes first"
-            },
-            "unique_action_id_2": {
-                ...
-                "condition": "executes only if unique_action_id_1 was successful"
-            },
-            ...
-        }
-
-    Returns:
-        The result of the plan execution.
-    """
-    logger.info("Executing plan", extra={"str_json_plan": str_json_plan})
-
-    agent_obj = agent.build_agent()
-    result = agent_obj.invoke(
+    agent_obj = await agent.get_agent()
+    result = await agent_obj.ainvoke(
         {
             "messages": [
                 SystemMessage(
-                    content="You are a helpful assistant that can execute plans. "
-                    "You are given a plan to execute. "
+                    content="""You are a helpful assistant that can execute plans.
+                    You are given a plan to execute. You are connected to the MCP registry where you
+                    can find possible MCP services and their tools to use in plan."""
                 ),
                 HumanMessage(content=str_json_plan),
             ]
-        }
+        },
+        config={"configurable": {"thread_id": thread_id}},
     )
 
-    logger.info("Plan executed", extra={"result": result})
+    logger.info(f"Plan executed, result={result}")
 
-    return result.content
+    return result
+
+
+@mcp_server.custom_route("/health", methods=["GET"])
+async def http_health_check(request):
+    return JSONResponse({"status": "healthy", "service": "mcp-agent-worker"})
 
 
 if __name__ == "__main__":
-    host = envs.MCP_HOST
-    port = int(envs.MCP_PORT)
-    logger.info("Starting MCP server", extra={"host": host, "port": port})
-    asyncio.run(mcp.run_async(transport="http", host=host, port=port))
+    logger.info(f"Starting MCP server on {MCP_HOST}:{MCP_PORT}")
+    mcp_server.run(transport="http", host=MCP_HOST, port=MCP_PORT)
